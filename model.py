39ba3efe46c45dd0cdb4e7a09cc62d795cd7b93c
Nguyen Quang Truong <139192880+QuangTruong-Nguyen@users.noreply.github.com>
2024-02-27 21:50:30 +0700

    Add files via upload

---
+++ model.py
@@ -0,0 +1,10 @@
+import lib
+
+class SiameseModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.backbone = InceptionResnetV1(pretrained='vggface2')
+    def forward(self, x):
+        x = self.backbone(x)
+        x = torch.nn.functional.normalize(x, dim=1)
+        return x
