2898fa8b5c7c6ff549c93740dc9e9fb759f0ccf7
Dai-Tai Quach <92372685+TaiQuach123@users.noreply.github.com>
2024-02-28 19:35:12 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,10 +1,16 @@
 from lib import *
 
 class SiameseModel(nn.Module):
-    def __init__(self):
+    def __init__(self, backbone='InceptionResnetV1'):
         super().__init__()
-        self.backbone = InceptionResnetV1(pretrained='vggface2')
+        if backbone=='InceptionResnetV1':
+            self.backbone = InceptionResnetV1(pretrained='vggface2')
     def forward(self, x):
         x = self.backbone(x)
-        x = torch.nn.functional.normalize(x, dim=1)
+        x = nn.functional.normalize(x, dim=1)
         return x
+
+if __name__ == "__main__":
+    model = SiameseModel()
+    rand_img = torch.rand((5,3,160,160))
+    print(model(rand_img).shape)
