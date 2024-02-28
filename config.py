2c15e0eac4dbf6446084b1d122a4f9ea988e32ae
Dai-Tai Quach <92372685+TaiQuach123@users.noreply.github.com>
2024-02-28 19:34:08 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-config = {
-    'partition_path': './PR_Project_Git/list_eval_partition.txt',
-    'img_path': './PR_project/img_align_celeba/',
-    'identity_path':'./PR_Project_Git/identity_CelebA.txt',
-    'img_size': (160,160),
-    'batch_size': 64,
-    'lr': 1e-4
-}
